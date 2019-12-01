00a65859e0413564135cfc76c578982753999243
Leonardo <leomatos@unidavi.edu.br>
2019-12-01 20:32:45 -0300

    script conexo + index

---
+++ conexao.php
@@ -0,0 +1,7 @@
+<?php
+
+$user = 'root';
+$pass = '';
+$dbh = new PDO('mysql:host=localhost;dbname=sistemaos', $user, $pass);
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include "conexao.php";
+    
+?>
+<link href="estilo.css" rel="stylesheet">
+
+<a class="cadastroproduto" href="cadastroproduto.php">Cadastrar Produtos</a>
+<br>
+<a class="cadastropessoa" href="cadastropessoa.php">Cadastrar Pessoas</a>
+<br>
+<a class="cadastroloja" href="cadastroloja.php">Cadastrar loja</a>
+<br>
+<a class="listagemproduto" href="listagemprodutos.php">Listagem de Produtos</a>
+<br>
+<a class="listagempessoas" href="listagempessoas.php">Listagem de Pessoas</a>
+<br>
+<a class="listagemproduto" href="cadastroloja.php">Listagem de Lojas</a>
+
+
